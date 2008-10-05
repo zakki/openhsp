993b30a6df6b4a2f6a470cbc45c451954c824c34
sharkpp <fuji.rosen@gmail.com>
2008-10-05 10:14:43 +0000

    間違えてローカルのCToken::ExpandFileの変更まで含めてコミットしてしまったので戻す。
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@184 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

--- hspcmp/token.cpp
+++ hspcmp/token.cpp
@@ -3169,18 +3169,6 @@ int CToken::ExpandLine( CMemBuf *buf, CMemBuf *src )
 }		
 
 
-#include <unistd.h>
-#include <limits.h>
-
-static int putfile( CMemBuf &fbuf, char *path, char *dir, char *dir2 ) {
-	char real[PATH_MAX];
-	realpath( path, real );
-	if ( strncmp( real, dir, strlen(dir) ) && strncmp( real, dir2, strlen(dir2) ) ) {
-		return 0;
-	}
-	return fbuf.PutFile( path ) >= 0;
-}
-
 int CToken::ExpandFile( CMemBuf *buf, char *fname, char *refname )
 {
 	//		ソースファイルをmembufへ展開する
@@ -3188,47 +3176,29 @@ int CToken::ExpandFile( CMemBuf *buf, char *fname, char *refname )
 	int res;
 	char mm[1024];
 	char cname[HSP_MAX_PATH];
-	char ext[HSP_MAX_PATH];
 	char purename[HSP_MAX_PATH];
 	char foldername[HSP_MAX_PATH];
-	char cwd[HSP_MAX_PATH];
 	CMemBuf fbuf;
 
-	getpath( fname, ext, 2 );
 	getpath( fname, purename, 8 );
 	getpath( fname, foldername, 32 );
-	
-	if ( strcmp(ext, ".hsp") && strcmp(ext, ".as") ) {
-		goto notfound;
-	}
-	
-	getcwd( cwd, HSP_MAX_PATH-1 );
-	strcat( cwd, "/" );
-	
 	if ( *foldername != 0 ) strcpy( search_path, foldername );
-	
-	if ( putfile( fbuf, fname, cwd, common_path ) ) { goto success; }
 
-	strcpy( cname, common_path );
-	strcat( cname, purename );
-
-	if ( putfile( fbuf, cname, cwd, common_path ) ) { goto success; }
-
-	strcpy( cname, search_path );
-	strcat( cname, purename );
-
-	if ( putfile( fbuf, cname, cwd, common_path ) ) { goto success; }
-
-	strcpy( cname, common_path );
-	strcat( cname, search_path );
-	strcat( cname, purename );
-
-	if ( putfile( fbuf, cname, cwd, common_path ) ) { goto success; }
-
-	goto notfound;
-
-success:
-	
+	if ( fbuf.PutFile( fname ) < 0 ) {
+		strcpy( cname, common_path );strcat( cname, purename );
+		if ( fbuf.PutFile( cname ) < 0 ) {
+			strcpy( cname, search_path );strcat( cname, purename );
+			if ( fbuf.PutFile( cname ) < 0 ) {
+				strcpy( cname, common_path );strcat( cname, search_path );strcat( cname, purename );
+				if ( fbuf.PutFile( cname ) < 0 ) {
+					if ( fileadd == 0 ) {
+						Mesf( "#Source file not found.[%s]",purename );
+					}
+					return -1;
+				}
+			}
+		}
+	}
 	fbuf.Put( (char)0 );
 
 	if ( fileadd ) {
@@ -3238,8 +3208,7 @@ success:
 	sprintf( mm,"\"%s\"",refname );
 	RegistExtMacroPath( "__file__", mm );			// ファイル名マクロを更新
 
-	sprintf( mm,"##0 \"%s\"\r\n",refname );
-	buf->PutStr( mm );
+	buf->PutStrf( "##0 \"%s\"\r\n",refname );
 
 	res = ExpandLine( buf, &fbuf );
 
@@ -3259,11 +3228,6 @@ success:
 		return -2;
 	}
 	return 0;
-notfound:
-	if ( fileadd == 0 ) {
-		Mesf( "#Source file not found.[%s]",purename );
-	}
-	return -1;
 }
 
 
