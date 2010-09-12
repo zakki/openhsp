f0ba152e35a62db84f0d5fd9ea3295f4946e1894
zakki <zakki@peppermint.jp>
2010-09-12 10:00:45 +0000

    hsp3cnvとhsp3rtestからコピー
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@389 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

---
+++ hsp3ll/main.cpp
@@ -0,0 +1,150 @@
+
+//
+//	HSP3CNV : HSP Code Converter Manager
+//				   onion software 2008/4
+//
+
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/Constants.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/Instructions.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/ExecutionEngine/Interpreter.h"
+#include "llvm/ExecutionEngine/GenericValue.h"
+#include "llvm/Target/TargetSelect.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <stdio.h>
+#include <windows.h>
+#include <direct.h>
+
+#include "chsp3.h"
+#include "chsp3rev.h"
+#include "chsp3cpp.h"
+#include "../hsp3/win32gui/hsp3win.h"
+
+
+using namespace llvm;
+
+//#define HSP3CNV_DEBUG			// デバッグモード用
+
+/*----------------------------------------------------------*/
+
+int GetFilePath( char *bname )
+{
+	//		フルパス名から、ファイルパスの取得(\を残す)
+	//
+	int a,b,len;
+	char a1;
+	b=-1;
+	len=(int)strlen(bname);
+	for(a=0;a<len;a++) {
+		a1=bname[a];
+		if (a1=='\\') b=a;
+		if (a1<0) a++; 
+	}
+	if (b<0) return 1;
+	bname[b+1]=0;
+	return 0;
+}
+
+/*----------------------------------------------------------*/
+
+void usage1( void )
+{
+static 	char *p[] = {
+	"usage: hsp3cnv [options] [filename]",
+	"       -o??? set output file to ???",
+	NULL };
+	int i;
+	for(i=0; p[i]; i++)
+		printf( "%s\n", p[i]);
+	return;
+}
+
+
+int readAx( char *fname )
+{
+	int st;
+	int cmpopt,ppopt;
+	//char fname[_MAX_PATH];
+	char fname2[_MAX_PATH];
+	char oname[_MAX_PATH];
+
+	//	check switch and prm
+
+	st = 0; ppopt = 0; cmpopt = 0;
+	fname[0]=0;
+	fname2[0]=0;
+	oname[0]=0;
+
+#ifndef HSP3CNV_DEBUG
+
+	int b;
+	char a1,a2;
+
+	if (fname[0]==0) { printf("No file name selected.\n");return 1; }
+#else
+	strcpy( fname,"test" );
+#endif
+
+	if (oname[0]==0) {
+		strcpy( oname,fname );addext( oname,"cpp" );
+	}
+	strcpy( fname2, fname ); addext( fname2,"hsp" );
+	addext( fname,"ax" );
+
+	//		call main
+	{
+		int i;
+		CHsp3Cpp hsp3;
+		i = hsp3.LoadObjectFile( fname );
+		if (i) {
+			printf( "File open error.[%s](%d)\n", fname, i );
+			return 1;
+		}
+		hsp3.MakeSource( 0, NULL );
+
+#ifndef HSP3CNV_DEBUG
+		hsp3.SaveOutBuf( oname );
+#else
+		hsp3.SaveOutBuf( "test.cpp" );
+		puts( hsp3.GetOutBuf() );
+#endif
+
+	}
+
+	return st;
+}
+
+
+/*----------------------------------------------------------*/
+
+int APIENTRY WinMain ( HINSTANCE hInstance,
+					   HINSTANCE hPrevInstance,
+					   LPSTR lpCmdParam,
+					   int iCmdShow )
+{
+  
+	InitializeNativeTarget();
+
+	LLVMContext Context;
+  
+	// Create some module to put our function into it.
+	Module *M = new Module("test", Context);
+
+	readAx(lpCmdParam);
+
+	int res;
+#ifdef HSPDEBUG
+	res = hsp3win_init( hInstance, lpCmdParam );
+#else
+	res = hsp3win_init( hInstance, NULL );
+#endif
+	if ( res == 0 ) {
+		res = hsp3win_exec();
+	}
+	return res;
+}
