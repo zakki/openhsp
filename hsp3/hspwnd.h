88728e8b76e5ab5ecac41c63deb5545ae6f90c97
onitama <onitama@onionsoft.net>
2011-05-23 11:56:27 +0000

    hspdish用の微修正
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@464 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

--- hsp3/hspwnd.h
+++ hsp3/hspwnd.h
@@ -5,21 +5,18 @@
 #ifndef __hspwnd_h
 #define __hspwnd_h
 
+#ifdef HSPDISH
+#include "../hsp3dish/hspwnd.h"
+#else
+
 #ifdef HSPWIN
 #include "win32gui/hspwnd_win.h"
-#endif
-
-#ifdef HSPIOS
-#include "ios/hspwnd_ios.h"
-#endif
-
-#ifdef HSPNDK
-#include "ndk/hspwnd_ndk.h"
 #endif
 
 #ifdef HSPLINUX
 #include "linux/hspwnd_linux.h"
 #endif
 
+#endif
 
 #endif
--- hsp3/supio.h
+++ hsp3/supio.h
@@ -5,6 +5,10 @@
 #ifndef __supio_h
 #define __supio_h
 
+#ifdef HSPDISH
+#include "../hsp3dish/supio.h"
+#else
+
 #ifdef HSPWIN
 #include "win32gui/supio_win.h"
 #endif
@@ -21,6 +25,7 @@
 #include "linux/supio_linux.h"
 #endif
 
+#endif
 
 #endif
 
