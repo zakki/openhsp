bfdf5731bbc23c98e7416a0a23743c21d6eae59b
onitama <onitama@onionsoft.net>
2014-04-25 12:14:32 +0000

    HSP3オフィシャル3D素材 "珠音（たまね）"をhgimg3/hgimg4フォルダに同梱
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@624 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

---
+++ package/win32/iOS/iHSP20admob/hsp3embed/misc.h
@@ -0,0 +1,23 @@
+#ifndef MISC_H
+
+#include <stdio.h>
+
+//#define ENABLE_DEBUGMESSAGE
+
+#ifdef ENABLE_DEBUGMESSAGE
+#define DebugMessage(...) DebugMessage0(__VA_ARGS__);
+#else
+#define DebugMessage(...) ;
+#endif
+
+void Alertf( const char* pszFormat, ...);
+void Alert( const char *msg );
+
+int sgn(int i);
+
+void strScan(FILE* fp, char* str);
+float floatScan(FILE* fp);
+int intScan(FILE* fp);
+
+#endif
+#define MISC_H
