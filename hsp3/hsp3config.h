fff3a7724411d74fb357905d93bd1b0e87ee0209
onitama <onitama@onionsoft.net>
2014-07-09 11:55:58 +0000

    ドキュメントの更新 hsp3ext.h削除の取り消し
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@633 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

--- hsp3/hsp3config.h
+++ hsp3/hsp3config.h
@@ -8,9 +8,9 @@
 //		システム関連ラベル
 //
 #define HSPTITLE "Hot Soup Processor ver."
-#define hspver "3.4beta5"
-#define mvscode 5		// minor version code
-#define vercode 0x3405	// version code
+#define hspver "3.4"
+#define mvscode 6		// minor version code
+#define vercode 0x3406	// version code
 
 #define HSPERR_HANDLE		// HSPエラー例外を有効にします
 #define SYSERR_HANDLE		// システムエラー例外を有効にします
