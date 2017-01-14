939623b5e1a40d73bc5c60c9034aca370c245e3c
onitama <onitama@ceb3ebad-0e53-d446-8596-a45b2b8d79cd>
2017-01-14 09:38:15 +0000

    gameplay-encoderの更新
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@830 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

--- hsp3dish/gameplay/encoder/src/TTFFontEncoder.h
+++ hsp3dish/gameplay/encoder/src/TTFFontEncoder.h
@@ -1,5 +1,6 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include "Font.h"
 
 #define START_INDEX     32
 #define END_INDEX       127
@@ -8,12 +9,14 @@
 namespace gameplay
 {
 
-// Structure of Glyph.
-class Glyph
+// Structure of TTF Glyph.
+class TTFGlyph
 {
 public:
     unsigned int index;
     unsigned int width;
+    int bearingX;
+    unsigned int advance;
     float uvCoords[4];
 };
 
@@ -22,12 +25,12 @@ public:
  * 
  * @param inFilePath Input file path to the tiff file.
  * @param outFilePath Output file path to write the gpb to.
- * @param fontSize Size of the font.
+ * @param fontSizes List of sizes to generate for the font.
  * @param id ID string of the font in the ref table.
  * @param fontpreview True if the pgm font preview file should be written. (For debugging)
  * 
  * @return 0 if successful, -1 if error.
  */
-int writeFont(const char* inFilePath, const char* outFilePath, unsigned int fontSize, const char* id, bool fontpreview);
+int writeFont(const char* inFilePath, const char* outFilePath, std::vector<unsigned int>& fontSize, const char* id, bool fontpreview, Font::FontFormat fontFormat);
 
 }
