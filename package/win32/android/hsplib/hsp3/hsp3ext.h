7388594d6cf9ab924c8fcc60d9b5c170ddaa4d87
onitama <onitama@onionsoft.net>
2019-12-12 11:03:11 +0000

    NDK版HSP3Dishの更新
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@1029 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

--- package/win32/android/hsplib/hsp3/hsp3ext.h
+++ package/win32/android/hsplib/hsp3/hsp3ext.h
@@ -6,7 +6,7 @@
 #define __hsp3ext_h
 
 #ifdef HSPWIN
-#include "win32gui/hsp3ext_win.h"
+#include "../hsp3/win32gui/hsp3ext_win.h"
 #endif
 
 #ifdef HSPIOS
@@ -19,10 +19,6 @@
 
 #ifdef HSPLINUX
 #include "linux/hsp3ext_linux.h"
-#endif
-
-#ifdef HSPEMSCRIPTEN
-#include "emscripten/hsp3ext_emscripten.h"
 #endif
 
 
