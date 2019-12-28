65e2783ef0006bbe904ab1c9adda739635cd936e
onitama <onitama@onionsoft.net>
2019-12-28 02:24:56 +0000

    android版HGIMG4の更新
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@1036 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

--- package/win32/android/hsplib/javafunc.h
+++ package/win32/android/hsplib/javafunc.h
@@ -25,6 +25,8 @@ int j_callAdMob( int val );
 int j_addWindowFlag( int val );
 int j_clearWindowFlag( int val );
 char *j_callFontBitmap( const char *in_str, int fontSize, int style, int *o_width, int *o_height );
+char *j_callFontBitmap_s( const char *in_str, int fontSize, int style, int *o_width, int *o_height, int *o_size, int *o_color );
+void j_callFontBitmap_e( void );
 
 char *j_getHttpInfo( void );
 int j_httpRequest( char *msg1, char *msg2, int type );
