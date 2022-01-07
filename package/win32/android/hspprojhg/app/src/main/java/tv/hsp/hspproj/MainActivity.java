ae8661619f03ac72902d33adaf3d2dbb7e3ae521
onitama <onitama@onionsoft.net>
2022-01-07 14:43:32 +0000

    android版ランタイムの更新
    git-svn-id: http://dev.onionsoft.net/svn/openhsp/trunk@1212 ceb3ebad-0e53-d446-8596-a45b2b8d79cd

---
+++ package/win32/android/hspprojhg/app/src/main/java/tv/hsp/hspproj/MainActivity.java
@@ -0,0 +1,14 @@
+package tv.hsp.hspproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
